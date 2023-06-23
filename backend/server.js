3eeb414e34a9ff5d134d7faf90f5c9c4449fe01c
beryl <racsi.hartono@pnep.co.id>
2023-06-23 15:38:39 +0700

    initial

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
+import itemRoutes from './routes/itemRoutes.js';
+import permissionRoutes from './routes/permissionRoutes.js';
+import userPermissionRoutes from './routes/userPermissionRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -19,18 +22,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
+app.use('/api/items', itemRoutes);
+app.use('/api/permissions', permissionRoutes);
+app.use('/api/userpermissions', userPermissionRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+	const __dirname = path.resolve();
+	app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+	);
 } else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+	app.get('/', (req, res) => {
+		res.send('API is running....');
+	});
 }
 
 app.use(notFound);
